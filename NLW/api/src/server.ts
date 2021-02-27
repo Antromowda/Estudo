e98bae75c4fb2a38bce13283e7defa5628d8754b
Enrico Palma <enricob.palma@gmail.com>
2021-02-26 22:13:34 -0300

    Estudo NLW DB

--- NLW/api/src/server.ts
+++ NLW/api/src/server.ts
@@ -1,30 +1,31 @@
-import 'reflect-metadata';
-import express from 'express';
-import './database';
+import "reflect-metadata";
+import express from "express";
+import "./database";
+import { router } from "./routes";
 
 const app = express();
 
-
-
-//GET => Buscar Info
+app.use(express.json()); // Necessário para o servidor o formato JSON. Formato TS JSON não é padrão.
+app.use(router);
+/* //GET => Buscar Info
 //POST => Salvar Info
 //PUT => Alterar Info
 //DELETE => Deletar
-//PATCH => Alteração específica 
+//PATCH => Alteração específica
 
 //http://localhost:3000/users
 app.get("/", (req, res) => {
-    //return res.send("Nudes")
-    return res.json({  message:"Hello World" });
+  //return res.send("Nudes")
+  return res.json({ message: "Hello World" });
 });
 
 // 1 param => Rota(Recurso API)
 // 2 param => req, res
 
 app.post("/", (req, res) => {
-    // Recebeu dados para serem salvos
-    return res.json({  message:"Os dados foram salvos com sucesso!" });
-    // "post" não coloca em json na página web
-});
+  // Recebeu dados para serem salvos
+  return res.json({ message: "Os dados foram salvos com sucesso!" });
+  // "post" não coloca em json na página web
+}); */
 
 app.listen(3000, () => console.log("Servidor rodando."));
